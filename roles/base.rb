2e6a79a6749e281c40e806c4e08361a239f21faf
Daniel Borcherding <daniel.borcherding@gmail.com>
2014-07-08 11:38:17 -0700

    Added chef-client:config to base role runlist

--- roles/base.rb
+++ roles/base.rb
@@ -1,3 +1,11 @@
 name "base"
 description "Base Server Role"
-run_list "recipe[chef-client::delete_validation]", "recipe[chef-client]", "recipe[ntp]", "recipe[motd]", "recipe[users]"
+run_list "recipe[chef-client::delete_validation]", "recipe[chef-client]",
+"recipe[chef-client::config]","recipe[ntp]", "recipe[motd]", "recipe[users]"
+default_attributes(
+  "chef-client" => {
+    "config" => {
+      "ssl_verify_mode" => ":verify_peer"
+    }
+  }
+)
